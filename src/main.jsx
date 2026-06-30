2f4adff8a248224eb0d30e87fb4dce883277704f
Sumit <sumitzeal4111@gmail.com>
2026-06-30 16:57:51 +0530

    Update website

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { MotionConfig } from "framer-motion";
+import DhitiSite from "./DhitiServices.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* Respects the visitor's "reduce motion" OS setting automatically */}
+    <MotionConfig reducedMotion="user">
+      <DhitiSite />
+    </MotionConfig>
   </React.StrictMode>
-)
+);
